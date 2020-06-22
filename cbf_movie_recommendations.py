1e0aee46a7b6e081f537c36f4588ca34c3b54f76
Jimmy Figueroa <av_jfigueroa@idcontractors.net>
2020-06-22 17:48:32 -0600

    cbf movie recommendations completed

--- cbf_movie_recommendations.py
+++ cbf_movie_recommendations.py
@@ -21,10 +21,31 @@ import warnings; warnings.simplefilter('ignore')
 # Movie Overviews and Taglines
 # Movie Cast, Crew, Keywords and Genre
 
-# movies
+# movies prep
 md = pd. read_csv('./data/movies_metadata.csv')
 md['genres'] = md['genres'].fillna('[]').apply(literal_eval).apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
 md['year'] = pd.to_datetime(md['release_date'], errors='coerce').apply(lambda x: str(x).split('-')[0] if x != np.nan else np.nan)
+
+vote_counts = md[md['vote_count'].notnull()]['vote_count'].astype('int')
+vote_averages = md[md['vote_average'].notnull()]['vote_average'].astype('int')
+C = vote_averages.mean()
+m = vote_counts.quantile(0.95)
+
+qualified = md[(md['vote_count'] >= m) & (md['vote_count'].notnull()) & (md['vote_average'].notnull())][['title', 'year', 'vote_count', 'vote_average', 'popularity', 'genres']]
+qualified['vote_count'] = qualified['vote_count'].astype('int')
+qualified['vote_average'] = qualified['vote_average'].astype('int')
+
+def weighted_rating(x):
+    v = x['vote_count']
+    R = x['vote_average']
+    return (v/(v+m) * R) + (m/(m+v) * C)
+
+qualified['wr'] = qualified.apply(weighted_rating, axis=1)
+qualified = qualified.sort_values('wr', ascending=False).head(250)
+s = md.apply(lambda x: pd.Series(x['genres']),axis=1).stack().reset_index(level=1, drop=True)
+s.name = 'genre'
+gen_md = md.drop('genres', axis=1).join(s)
+
 
 # Movie Overviews and Taglines content base fltering
 links_small = pd.read_csv('./data/links_small.csv')
@@ -66,4 +87,100 @@ def get_recommendations(title):
     movie_indices = [i[0] for i in sim_scores]
     return titles.iloc[movie_indices]
 
+# metadata based recommender
+credits = pd.read_csv('./data/credits.csv')
+keywords = pd.read_csv('./data/keywords.csv')
 
+keywords['id'] = keywords['id'].astype('int')
+credits['id'] = credits['id'].astype('int')
+md['id'] = md['id'].astype('int')
+
+print("::: md shape = {}".format(md.shape))
+
+md = md.merge(credits, on='id')
+md = md.merge(keywords, on='id')
+
+smd = md[md['id'].isin(links_small)]
+print("::: smd shape = {}".format(smd.shape))
+
+# We now have our cast, crew, genres and credits, all in one dataframe
+smd['cast'] = smd['cast'].apply(literal_eval)
+smd['crew'] = smd['crew'].apply(literal_eval)
+smd['keywords'] = smd['keywords'].apply(literal_eval)
+smd['cast_size'] = smd['cast'].apply(lambda x: len(x))
+smd['crew_size'] = smd['crew'].apply(lambda x: len(x))
+
+def get_director(x):
+    for i in x:
+        if i['job'] == 'Director':
+            return i['name']
+    return np.nan
+
+smd['director'] = smd['crew'].apply(get_director)
+smd['cast'] = smd['cast'].apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+smd['cast'] = smd['cast'].apply(lambda x: x[:3] if len(x) >=3 else x)
+smd['keywords'] = smd['keywords'].apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+
+smd['cast'] = smd['cast'].apply(lambda x: [str.lower(i.replace(" ", "")) for i in x])
+smd['director'] = smd['director'].astype('str').apply(lambda x: str.lower(x.replace(" ", "")))
+smd['director'] = smd['director'].apply(lambda x: [x,x, x])
+
+s = smd.apply(lambda x: pd.Series(x['keywords']),axis=1).stack().reset_index(level=1, drop=True)
+s.name = 'keyword'
+
+s = s.value_counts()
+print("::: s value counts {}".format(s[:5]))
+
+s = s[s > 1]
+
+stemmer = SnowballStemmer('english')
+stemmer.stem('dogs')
+
+def filter_keywords(x):
+    words = []
+    for i in x:
+        if i in s:
+            words.append(i)
+    return words
+
+smd['keywords'] = smd['keywords'].apply(filter_keywords)
+smd['keywords'] = smd['keywords'].apply(lambda x: [stemmer.stem(i) for i in x])
+smd['keywords'] = smd['keywords'].apply(lambda x: [str.lower(i.replace(" ", "")) for i in x])
+
+smd['soup'] = smd['keywords'] + smd['cast'] + smd['director'] + smd['genres']
+smd['soup'] = smd['soup'].apply(lambda x: ' '.join(x))
+
+count = CountVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
+count_matrix = count.fit_transform(smd['soup'])
+
+cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+smd = smd.reset_index()
+titles = smd['title']
+indices = pd.Series(smd.index, index=smd['title'])
+
+# get_recommendations('The Dark Knight').head(10)
+# get_recommendations('Mean Girls').head(10)
+
+
+def improved_recommendations(title):
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:26]
+    movie_indices = [i[0] for i in sim_scores]
+
+    movies = smd.iloc[movie_indices][['title', 'vote_count', 'vote_average', 'year']]
+    vote_counts = movies[movies['vote_count'].notnull()]['vote_count'].astype('int')
+    vote_averages = movies[movies['vote_average'].notnull()]['vote_average'].astype('int')
+    C = vote_averages.mean()
+    m = vote_counts.quantile(0.60)
+    qualified = movies[
+        (movies['vote_count'] >= m) & (movies['vote_count'].notnull()) & (movies['vote_average'].notnull())]
+    qualified['vote_count'] = qualified['vote_count'].astype('int')
+    qualified['vote_average'] = qualified['vote_average'].astype('int')
+    qualified['wr'] = qualified.apply(weighted_rating, axis=1)
+    qualified = qualified.sort_values('wr', ascending=False).head(10)
+    return qualified
+
+improved_recommendations('The Dark Knight')
