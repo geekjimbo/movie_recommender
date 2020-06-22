fb5abfe6edd9f03c4b41e44dfa5e6bef0623c37b
Jimmy Figueroa <av_jfigueroa@idcontractors.net>
2020-06-22 17:27:22 -0600

    cbf based on movie description and taglines completed

--- cbf_movie_recommendations.py
+++ cbf_movie_recommendations.py
@@ -9,7 +9,8 @@ from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
 from nltk.stem.snowball import SnowballStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import wordnet
-from surprise import Reader, Dataset, SVD, evaluate
+from surprise import Reader, Dataset, SVD
+# from surprise import Reader, Dataset, SVD, evaluate
 
 import warnings; warnings.simplefilter('ignore')
 
@@ -20,8 +21,13 @@ import warnings; warnings.simplefilter('ignore')
 # Movie Overviews and Taglines
 # Movie Cast, Crew, Keywords and Genre
 
+# movies
+md = pd. read_csv('./data/movies_metadata.csv')
+md['genres'] = md['genres'].fillna('[]').apply(literal_eval).apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+md['year'] = pd.to_datetime(md['release_date'], errors='coerce').apply(lambda x: str(x).split('-')[0] if x != np.nan else np.nan)
+
 # Movie Overviews and Taglines content base fltering
-links_small = pd.read_csv('../input/links_small.csv')
+links_small = pd.read_csv('./data/links_small.csv')
 links_small = links_small[links_small['tmdbId'].notnull()]['tmdbId'].astype('int')
 
 md = md.drop([19730, 29503, 35587])
@@ -39,5 +45,25 @@ tf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='en
 tfidf_matrix = tf.fit_transform(smd['description'])
 
 print("::: tfidf matrix shape = {}".format(tfidf_matrix.shape))
+
+# Since we have used the TF-IDF Vectorizer,
+# calculating the Dot Product will directly give us the Cosine Similarity Score.
+# Therefore, we will use sklearn's linear_kernel instead of cosine_similarities since it is much faster.
+
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+print("::: cosine sim 0 = {}".format(cosine_sim[0]))
+
+# function to get the 20 most similar movies based on cosine similarity
+smd = smd.reset_index()
+titles = smd['title']
+indices = pd.Series(smd.index, index=smd['title'])
+
+def get_recommendations(title):
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:21]
+    movie_indices = [i[0] for i in sim_scores]
+    return titles.iloc[movie_indices]
 
 
