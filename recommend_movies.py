548fb251853aa5b6bfd7bd2c9394d90b5946180c
Jimmy Figueroa <av_jfigueroa@idcontractors.net>
2020-06-23 09:05:22 -0600

    avroconsumer in place

--- recommend_movies.py
+++ recommend_movies.py
@@ -1,8 +1,38 @@
+from confluent_kafka.avro import AvroConsumer
+from confluent_kafka.avro.serializer import SerializerError
 import lib.cbf_movie_recommendations as cbfmr
 
-query_title = 'The Dark Knight'
-recommendations = cbfmr.movie_recommendations(query_title)
-response = {"query_title": query_title, "recommended_movies": recommendations}
+def recommed_movies(query_title):
+    recommendations = cbfmr.movie_recommendations(query_title)
+    response = {"query_title": query_title, "recommended_movies": recommendations}
+    return response
 
-print("::: test result")
-print(response)
+c = AvroConsumer({
+    'bootstrap.servers': '0.0.0.0:9092',
+    'group.id': 'group01',
+    'schema.registry.url': 'http://0.0.0.0:8081'})
+
+c.subscribe(['movies'])
+
+while True:
+    try:
+        msg = c.poll(10)
+
+    except SerializerError as e:
+        print("Message deserialization failed for {}: {}".format(msg, e))
+        break
+
+    if msg is None:
+        continue
+
+    if msg.error():
+        print("AvroConsumer error: {}".format(msg.error()))
+        continue
+
+    query_title = msg.value()['query_title']
+    print(query_title)
+    recommendations = recommed_movies(query_title)
+    print(type(recommendations))
+    print(recommendations)
+
+c.close()
